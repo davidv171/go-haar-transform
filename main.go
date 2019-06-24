e1175958ec62a6572ca88f1d53f3f75fd27d1510
davidv171 <david.vuckovic7@gmail.com>
2019-06-24 22:15:16 +0200

    Check for compression

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"golang.org/x/image/bmp"
 	"os"
 	"strconv"
+    "math"
 )
 
 func main() {
@@ -14,36 +15,41 @@ func main() {
 	input := os.Args[2]
 	output := os.Args[3]
 	//threshold for compression
-	thr, err := strconv.ParseFloat(os.Args[4], 8)
+	thr, err := strconv.ParseFloat(os.Args[4], 32)
 	errCheck(err)
-	f, err := os.Open(input)
-	errCheck(err)
-	r := bufio.NewReader(f)
-	fi, err := f.Stat()
-	errCheck(err)
-	fmt.Println("The file is %d bytes long before performing HAAR ", fi.Size())
-	btmp, err := bmp.Decode(r)
-	errCheck(err)
-	pixels := make([][]float32, btmp.Bounds().Size().X)
-	fmt.Println("Bitmap dimensions : ", btmp.Bounds().Size())
-	for i := 0; i < btmp.Bounds().Size().X; i++ {
-		pixels[i] = make([]float32, btmp.Bounds().Size().Y)
-		for j := 0; j < btmp.Bounds().Size().Y; j++ {
-			pix, _, _, _ := btmp.At(i, j).RGBA()
-			//we're dealing with n bit depth gray pixel, the library always does 0-65635
-			pix = pix >> 8
-			pixels[i][j] = float32(pix)
+	if op == "c" {
+		f, err := os.Open(input)
+		errCheck(err)
+		r := bufio.NewReader(f)
+		fi, err := f.Stat()
+		errCheck(err)
+		fmt.Println("The file is %d bytes long before performing HAAR ", fi.Size())
+		btmp, err := bmp.Decode(r)
+		errCheck(err)
+		pixels := make([][]float32, btmp.Bounds().Size().X)
+		fmt.Println("Bitmap dimensions : ", btmp.Bounds().Size())
+		for i := 0; i < btmp.Bounds().Size().X; i++ {
+			pixels[i] = make([]float32, btmp.Bounds().Size().Y)
+			for j := 0; j < btmp.Bounds().Size().Y; j++ {
+				pix, _, _, _ := btmp.At(i, j).RGBA()
+				//we're dealing with n bit depth gray pixel, the library always does 0-65635
+				pix = pix >> 8
+				pixels[i][j] = float32(pix)
+			}
 		}
+		//We assume a symmetric BMP
+		haar := blocks(pixels, btmp.Bounds().Size().X, btmp.Bounds().Size().Y, float32(thr))
+		//Prepend
+		//data = append([]string{"Prepend Item"}, data...)
+		by := float32ToBytes(haar)
+		bound := math.Log2(float64(btmp.Bounds().Size().X))
+        byteBound := byte(bound)
+		by = append(byteBound, by...)
+		fmt.Println("Haar length", len(by))
+		file, err := os.OpenFile(output, os.O_RDWR, 0644)
+		defer file.Close()
+		errCheck(err)
+		_, err = file.Write(by)
+		errCheck(err)
 	}
-	//We assume a symmetric BMP
-	haar := blocks(pixels, btmp.Bounds().Size().X, btmp.Bounds().Size().Y, float32(thr))
-	//Prepend
-	//data = append([]string{"Prepend Item"}, data...)
-	by := float32ToBytes(haar)
-	bound := Int32ToBytes(int32(btmp.Bounds().Size().X))
-	by = append(bound, by...)
-	fmt.Println("Haar length", len(by))
-	Arithmetic(op, output, by)
-
-	os.Exit(0)
 }
